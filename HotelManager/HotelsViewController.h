f5e9416c1116698452f8f0cc38249016c268bf9c
Alberto-Vega <el_vega@live.com>
2015-11-30 17:09:35 -0800

    Added Buttons and constraints

---
+++ HotelManager/HotelsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotelsViewController.h
+//  HotelManager
+//
+//  Created by Alberto Vega Gonzalez on 11/30/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelsViewController : UIViewController
+
+@end
