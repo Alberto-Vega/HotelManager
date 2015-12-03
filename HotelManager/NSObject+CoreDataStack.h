94e6e807f109cbe77b084a8395e7629fdb3fe96a
Alberto-Vega <el_vega@live.com>
2015-12-02 17:06:10 -0800

    Committing before creating a new branch

---
+++ HotelManager/NSObject+CoreDataStack.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+CoreDataStack.h
+//  HotelManager
+//
+//  Created by Alberto Vega Gonzalez on 12/2/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (CoreDataStack)
+
+@end
---
+++ HotelManager/NSObject+CoreDataStack.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+CoreDataStack.m
+//  HotelManager
+//
+//  Created by Alberto Vega Gonzalez on 12/2/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import "NSObject+CoreDataStack.h"
+
+@implementation NSObject (CoreDataStack)
+
+@end
